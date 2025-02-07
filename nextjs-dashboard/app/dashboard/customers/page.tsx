3a2fe23bab5f3b2f722275117f8992f02e04bbe5
divik17dhiman <divik17dhiman@gmail.com>
2025-02-07 17:23:13 +0530

    fixed bugs

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: 'Customers',
+  };
+
+export default function Customer(){
+    return <p>Paaji Randi</p>
+}
--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,6 +5,11 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices',
+};
  
 export default async function Page(props: {
     searchParams?: Promise<{
@@ -28,7 +33,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        {/*<Pagination totalPages={totalPages} />*/}
       </div>
     </div>
   );
