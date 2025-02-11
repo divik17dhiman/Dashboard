6de1f50025bc2c74a791523765549f700e354a3b
divik17dhiman <divik17dhiman@gmail.com>
2025-02-11 17:04:09 +0530

    added pagination

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { Metadata } from 'next';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 export const metadata: Metadata = {
     title: 'Invoices',
@@ -20,6 +21,7 @@ export default async function Page(props: {
     const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchInvoicesPages(query);
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -33,7 +35,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/*<Pagination totalPages={totalPages} />*/}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
